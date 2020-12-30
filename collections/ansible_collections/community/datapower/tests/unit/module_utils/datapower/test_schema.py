0b93ab842758b78be4184d4b91f988a4df7023cd
Anthony Schneider <tonyschndr@gmail.com>
2020-12-30 14:17:09 -0600

    updated schema and testing so tests can load from schema/

--- collections/ansible_collections/community/datapower/tests/unit/module_utils/datapower/test_schema.py
+++ collections/ansible_collections/community/datapower/tests/unit/module_utils/datapower/test_schema.py
@@ -5,7 +5,7 @@ __metaclass__ = type
 import re
 
 import pytest
-
+import json
 from ansible_collections.community.datapower.plugins.module_utils.datapower import (
     dp_diff
 )
@@ -19,21 +19,17 @@ from ansible_collections.community.datapower.tests.unit.module_utils.test_data i
 )
 class TestDPManageConfigObject:
     
-    def setup_method(self, test_method):
-        self.valcred_schema = DPManageConfigSchema(test_data.valcred_schema_response)
-        self.eth_schema = DPManageConfigSchema(test_data.schema_ethernet_interface)
-        self.aaa_schema = DPManageConfigSchema(test_data.aaa_policy_schema_response)
     
-    def teardown_method(self, test_method):
-        del self.valcred_schema
-        del self.eth_schema
-        del self.aaa_schema
+    def load_schema(self, class_):
+        with open('tests/unit/module_utils/test_data/schema/' + class_ +'.json') as f:
+            data = json.loads(f.read())
+        return DPManageConfigSchema(data['schema'])
 
     def test_DPManageConfigSchema(self):
         assert DPManageConfigSchema(test_data.valcred_schema_response)
         
     def test_DPManageConfigSchema_get_prop_1(self):   
-        dp_schema = self.valcred_schema
+        dp_schema = self.load_schema('CryptoValCred')
         assert dp_schema.get_prop('Certificate') == {
             "array": "true",
             "cli-alias": "certificate",
@@ -51,7 +47,7 @@ class TestDPManageConfigObject:
         assert dp_schema.get_prop('nothing') == None
 
     def test_DPManageConfigSchema_get_prop_2(self):
-        dp_schema = self.aaa_schema
+        dp_schema = self.load_schema('AAAPolicy')
         assert dp_schema.get_prop('AZCacheTTL') == {
             "name": "AZCacheTTL",
             "default": 3,
@@ -74,31 +70,31 @@ class TestDPManageConfigObject:
         }
 
     def test_DPManageConfigSchema_get_type_href_from_prop_1(self):   
-        dp_schema = self.valcred_schema
+        dp_schema =self.load_schema('CryptoValCred')
         prop = dp_schema.get_prop('Certificate')
         href = dp_schema.get_type_href_from_prop(prop)
         assert href == '/mgmt/types/default/dmReference'
 
     def test_DPManageConfigSchema_get_type_href_from_prop_2(self):  
-        dp_schema = self.valcred_schema 
+        dp_schema = self.load_schema('CryptoValCred') 
         prop = dp_schema.get_prop('mAdminState')
         href = dp_schema.get_type_href_from_prop(prop)
         assert href == "/mgmt/types/default/dmAdminState"
 
     def test_DPManageConfigSchema_get_type_href_from_prop_3(self):  
-        dp_schema = self.valcred_schema
+        dp_schema = self.load_schema('CryptoValCred')
         prop = dp_schema.get_prop('InitialPolicySet')
         href = dp_schema.get_type_href_from_prop(prop)
         assert href == '/mgmt/types/default/dmString'
     
     def test_DPManageConfigSchema_get_type_href_from_prop_4(self):  
-        dp_schema = self.aaa_schema
+        dp_schema = self.load_schema('AAAPolicy')
         prop = dp_schema.get_prop('AZCacheTTL')
         href = dp_schema.get_type_href_from_prop(prop)
         assert href == '/mgmt/types/default/dmUInt32'
 
     def test_DPManageConfigSchema_get_type_1 (self):   
-        dp_schema = self.valcred_schema
+        dp_schema = self.load_schema('CryptoValCred')
         assert dp_schema.get_type('/mgmt/types/default/dmString') == {
                 "_links": {
                     "doc": {
@@ -115,7 +111,7 @@ class TestDPManageConfigObject:
             }
 
     def test_DPManageConfigSchema_get_type_2(self):
-        dp_schema = self.aaa_schema
+        dp_schema = self.load_schema('AAAPolicy')
 
         type_ = {
             "_links": {
@@ -136,51 +132,51 @@ class TestDPManageConfigObject:
         assert dp_schema.get_type('/mgmt/types/default/dmUInt32') == type_
 
     def test_DPManageConfigSchema_get_is_valid_param_1(self):
-        dp_schema = self.valcred_schema
+        dp_schema = self.load_schema('CryptoValCred')
         assert dp_schema.is_valid_param('mAdminState', 'disabled')
 
     def test_DPManageConfigSchema_get_is_valid_param_2(self):
-        dp_schema = self.valcred_schema
+        dp_schema = self.load_schema('CryptoValCred')
         assert dp_schema.is_valid_param('Certificate', {'value': 'Test1'})
 
 
     def test_DPManageConfigSchema_get_is_valid_param_3(self):
-        dp_schema = self.valcred_schema
+        dp_schema = self.load_schema('CryptoValCred')
         assert dp_schema.is_valid_param('Certificate', {'Value': 'Test1'}) == False
 
     def test_DPManageConfigSchema_get_is_valid_param_4(self):
-        dp_schema = self.valcred_schema
+        dp_schema = self.load_schema('CryptoValCred')
         assert dp_schema.is_valid_param('ExplicitPolicy', 'on')
 
     def test_DPManageConfigSchema_get_is_valid_param_5(self):
-        dp_schema = self.valcred_schema
+        dp_schema = self.load_schema('CryptoValCred')
         assert dp_schema.is_valid_param('ExplicitPolicy', 'foo') == False
         
     def test_DPManageConfigSchema_get_is_valid_param_6(self):
-        dp_schema = self.valcred_schema
+        dp_schema = self.load_schema('CryptoValCred')
         assert dp_schema.is_valid_param('InitialPolicySet', 'anystring')
 
     def test_DPManageConfigSchema_get_is_valid_param_7(self):
-        dp_schema = self.valcred_schema
+        dp_schema = self.load_schema('CryptoValCred')
         assert dp_schema.is_valid_param('InitialPolicySet', 2) == False
 
     def test_DPManageConfigSchema_get_is_valid_param_8(self):
-        dp_schema = self.eth_schema
+        dp_schema = self.load_schema('EthernetInterface')
         assert dp_schema.is_valid_param('IPAddress', '10.0.1.1')
 
     def test_DPManageConfigSchema_get_is_valid_param_9(self):
-        dp_schema = self.aaa_schema
+        dp_schema = self.load_schema('AAAPolicy')
         assert dp_schema.is_valid_param('ExtractIdentity', test_data.AAAPolicy_ExtractIdentity)
 
     def test_DPManageConfigSchema_get_is_valid_param_10(self):
-        dp_schema = self.eth_schema
+        dp_schema = self.load_schema('EthernetInterface')
         assert dp_schema.is_valid_param('mAdminState', 'disabled')
     
     def test_DPManageConfigSchema_get_is_valid_param_nested_prop_in_type(self):
-        dp_schema = self.aaa_schema
+        dp_schema = self.load_schema('AAAPolicy')
         assert dp_schema.is_valid_param('AZCacheTTL', 3)
 
     def test_DPManageConfigSchema_schema_get_type_int_boundries(self):
         type_ = test_data.int_type
-        dp_schema = self.eth_schema
+        dp_schema = self.load_schema('EthernetInterface')
         assert dp_schema.get_type_int_boundries(type_) == (0, 65535)
--- process_schema.py
+++ process_schema.py
@@ -3,18 +3,12 @@ import json
 
 with open('all_dp_obj_schema.json') as f:
     data = json.loads(f.read())
+    path = './collections/ansible_collections/community/datapower/tests/unit/module_utils/test_data/schema/'
     if 'results' in data: 
         for result in data['results']:
             if result['invocation']['module_args']['class_name'] == 'self':
                 continue
             filename = result['invocation']['module_args']['class_name'] + '.json'
             data = result['result']
-            path = './schema/'
             with open(path + filename, 'w') as fp:
                 json.dump(data, fp, indent=4)
-    else:
-        filename = 'AAAPolicy' + '.json'
-        data = data['result']
-        path = './schema/'
-        with open(path + filename, 'w') as fp:
-            json.dump(data, fp, indent=4)

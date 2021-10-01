85f7b8d6305a47b1e6f8200379dc57f2a3a9d8d3
Anthony Schneider <tonyschndr@gmail.com>
2021-09-30 21:21:41 -0500

    save..

--- ansible_collections/community/datapower/plugins/httpapi/rest_mgmt.py
+++ ansible_collections/community/datapower/plugins/httpapi/rest_mgmt.py
@@ -1,10 +1,12 @@
 
 from __future__ import absolute_import, division, print_function
+from ansible_collections.community.datapower.plugins.module_utils.datapower.requests import MGMT_CONFIG_URI
 from ansible.plugins.httpapi import HttpApiBase
 from ansible.module_utils.six.moves.urllib.error import HTTPError, URLError
 from ansible.module_utils.connection import ConnectionError
 from ansible.module_utils._text import to_text
 import json
+import posixpath
 
 __metaclass__ = type
 
@@ -16,9 +18,12 @@ description:
 - This HttpApi plugin provides methods to connect to IBM DataPower REST management interface.
 version_added: 1.0.0
 """
+NO_BASE_PATH_ERROR = 'Base path was not provided. ie /mgmt/config/'
 
+MGMT_CONFIG_URI = '/mgmt/config/'
 
 class HttpApi(HttpApiBase):
+
     def send_request(self, path, method, data):
         if data:
             data = json.dumps(data)
@@ -31,10 +36,35 @@ class HttpApi(HttpApiBase):
         )
         return handle_response(response, response_data)
 
+    def edit_config(self, commands):
+        for command in commands:
+            self.send_request(**command)
+
+    def get_config(self, domain, class_name):
+        path = join_path(domain, class_name, base_path=MGMT_CONFIG_URI)
+        return self.send_request(path, 'GET', None)
+
+
+
+    # TODO: What capabilites should be returned from datapower?
+    def get_capabilities(self):
+        return '{"rest_mgmt" : ""}'
+
+
+def join_path(*args, base_path=None):
+    ''' Join the path to form the full URI
+    args -- list to join with base path, composes the right half of the URI
+    base_path -- string representing the base uri of the rest mgmt interface call, ie /mgmt/config/
+    '''
+    if not base_path:
+        raise ValueError(NO_BASE_PATH_ERROR)
+    path = '/'.join([arg for arg in args if arg is not None]).rstrip('/')
+    return posixpath.join(base_path, path)
 
 def handle_response(response, response_data):
     try:
         response_data = json.loads(response_data.read())
+
     except ValueError:
         response_data = response_data.read()
 
@@ -52,3 +82,6 @@ def handle_response(response, response_data):
         raise ConnectionError(to_text(response), code=response.code)
 
     return response_data
+
+
+
